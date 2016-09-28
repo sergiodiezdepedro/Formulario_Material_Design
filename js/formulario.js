fec4d793dc56db6f0c6c176a86ddeb9591ddf23e
sergiodiezdepedro <sergio.d.depedro@gmail.com>
2016-09-28 23:53:24 +0200

    Ligeras modificaciones de carácter logístico

--- js/formulario.js
+++ js/formulario.js
@@ -9,7 +9,7 @@
      // Identificamos si el elemento es de tipo texto, email, password, radio o checkbox
             if (elementos[i].type == "text" || elementos[i].type == "email" || elementos[i].type == "password") {
     // Si es tipo texto, email o password vamos a comprobar que esten completados los input
-                if (elementos[i].value == 0) {
+                if (elementos[i].value === 0) {
                     console.log('El campo ' + elementos[i].name + ' está incompleto');
                     elementos[i].className = elementos[i].className + ' error';
                     return false;
@@ -45,7 +45,7 @@
                         break;
                     }
                 }
-                   if(resultado == false) {
+                   if(resultado === false) {
                        elementos[i].parentNode.className = elementos[i].parentNode.className + " error";
                        console.log('El campo sexo está incompleto');
                        return false;
@@ -71,9 +71,9 @@
 				}
 			}
 
-			if (resultado == false) {
+			if (resultado === false) {
 				elementos[i].parentNode.className = elementos[i].parentNode.className + " error";
-				console.log('Falta por aceptar los términos');
+				console.log('Falta por aceptar lo;s términos');
 				return false;
 			} else {
 				// Eliminamos la clase Error del checkbox
@@ -118,7 +118,7 @@
         if (elementos[i].type == "text" || elementos[i].type == "email" || elementos[i].type == "password") {
             elementos[i].addEventListener("focus", focusInput);
             elementos[i].addEventListener("blur", blurInput);
-         };
+         }
     }
 
-}())
+}());
